e038650197869b9ba70ec3a8376f24d6acb370bd
JOAO PEDRO RODRIGUES DE ARAUJO <joao.praraujo3@senacsp.edu.br>
2026-06-19 14:31:25 -0300

    Adicionando tudo

--- Banco de Dados/Banco de Dados Clothean.sql
+++ Banco de Dados/Banco de Dados Clothean.sql
@@ -5,15 +5,15 @@ CREATE TABLE tbl_Usuario
 (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
 	cpf CHAR(14) NOT NULL,
-	telefone CHAR(11) NOT NULL,
+	telefone CHAR(15) NOT NULL,
 	senha VARCHAR(255) NOT NULL,
-    perfil VARCHAR(30) DEFAULT 'Usuario'
+    perfil ENUM('Adm', 'Usuario') DEFAULT 'Usuario'  NOT NULL
 );
 
-INSERT INTO Tbl_Usuario (nome, email, cpf, telefone, senha) VALUES 
-('Otávio', 'otavio.augusttocc@gmail.com', '50916371840', '11992181212', 'Ota_01');
+INSERT INTO Tbl_Usuario (nome, email, cpf, telefone, senha, perfil) VALUES 
+('Otávio', 'otav@gmail.com', '50916371840', '(11) 99218-1213', 'Ota_01', 'Adm');
 
 SELECT * FROM tbl_Usuario
 WHERE email = 'otavio.augusttocc@gmail.com';
