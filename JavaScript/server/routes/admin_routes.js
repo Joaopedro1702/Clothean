40cab6378b3060aca4fbe8c33bc4f6e3801a0d54
JOAO PEDRO RODRIGUES DE ARAUJO <joao.praraujo3@senacsp.edu.br>
2026-06-24 15:56:29 -0300

    Fix caminhos no user_routes novamente

--- JavaScript/server/routes/admin_routes.js
+++ JavaScript/server/routes/admin_routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-const db = require("../conexao");
-const { verificarToken } = require("../autorizacao");
+const db = require("../../BackEnd/DataBase/");
+const { verificarToken } = require("../../middleware/autorizacao");
 
 async function permitirPrimeiroAdmin(req, res, next) {
     try {
